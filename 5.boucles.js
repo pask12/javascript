3e2a7efd085a441526400d2d7f287d8f5247ae83
pascal <pascal.baudel@yahoo.fr>
2024-03-22 11:26:16 +0100

    boucles

---
+++ 5.boucles.js
@@ -0,0 +1,53 @@
+//exo 1
+
+// let n;
+
+
+// do {
+
+//   console.log("Entrez un nombre entre 1 et 3");
+
+//   n = prompt("Entrez un nombre");
+
+
+//   if (n < 1 || n > 3) {
+
+//     console.log("Saisie erronée. Recommencez");
+
+//     alert("Saisie erronée. Recommencez");
+//   }
+
+// } while (n < 1 || n > 3);
+
+// alert("Saisie correct");
+
+
+
+// exo 2
+
+let n;
+
+while(true){
+    n=prompt("entrez un nombre entre 10 et 20");
+
+    if(n<10 || n>20){
+
+    if(n <10){
+        alert("plus grand!");
+        }
+        else {(n>20);
+        alert("Plus petit!");
+        }
+
+    }else{
+alert("correct");
+    
+break;
+ }
+}
+
+
+    
+
+
+    
